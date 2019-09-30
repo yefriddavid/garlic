d8420d1a4b62cff8a60da6cb3daa948409b08938
dave <yefriddavid@gmail.com>
2019-09-30 01:33:31 -0300

    polish cliten node

--- client/client.js
+++ client/client.js
@@ -1,8 +1,8 @@
 // const protoLoader = require('@grpc/proto-loader');
-
 const grpc = require('grpc')
-const PROTO_PATH = '../notificator/pkg/grpc/pb/notificator.proto'
 const protoLoader = require('@grpc/proto-loader')
+
+const PROTO_PATH = '../notificator/pkg/grpc/pb/notificator.proto'
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);
 const NotificatorService = grpc.loadPackageDefinition(packageDefinition).pb.Notificator;
 const client = new NotificatorService("127.0.0.1:8802", grpc.credentials.createInsecure());
